fb77e1139637d30aa65ad5b288687e984ae88a4b
Izaak Walton <izaakw@protonmail.com>
2022-02-14 18:14:48 -0700

    mucking in assembly

---
+++ peuler001.asm
@@ -0,0 +1,9 @@
+	;; Project Euler # 1, translated from gcc C output
+
+	push	rbp
+	mov	rbp, rsp
+	sub	rsp, 0x10
+	mov	DWORD PTR [rbp-0x8], 0x0
+	mov	DWORD PTR [rbp-0x8], 0x3
+	jmp	0x11c9 <main+128>
+	mov	ecx,DWORD PTR [rbp-0x4]
